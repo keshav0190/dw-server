9faa25b9c3090a4f5b6be79c04e298b368913fb7
Keshav Methar <keshav.methar@axzora.com>
2023-01-17 12:34:01 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const cors = require("cors");
 const router = require('./routes');
 
 const app = express();
 app.use(express.json());
+var corsOptions = {
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+};
+app.use(cors(corsOptions));
 app.use(router);
-
+app.use(express.json());
 app.listen(8080, () => console.log('server listening on port 8080'));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Test." });
+  });
 
